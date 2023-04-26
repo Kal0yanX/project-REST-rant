e3081356eadbba9c4636db0b8a607880c0c50ee3
Juan Mireles <119257668+Kal0yanX@users.noreply.github.com>
2023-04-26 18:25:16 -0400

    fixed the error on index.js

--- controllers/places.js
+++ controllers/places.js
@@ -1,12 +1,10 @@
-const { application } = require('express')
-
 const router = require('express').Router()
 
-//More code here in a moment
 router.get('/', (req, res) => {
-    res.send('GET/places')
+    res.send('GET /places')
 })
 
 module.exports = router
+
 
 
