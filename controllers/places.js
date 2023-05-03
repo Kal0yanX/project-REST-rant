3d202a4362627375b01a799fc9d7847a4706dd76
Juan Mireles <119257668+Kal0yanX@users.noreply.github.com>
2023-05-03 10:58:12 -0400

    Show route connected

--- controllers/places.js
+++ controllers/places.js
@@ -21,11 +21,26 @@ router.post('/', (req, res) => {
   res.redirect('/places')
 })
 
-
-
 // GET /places
 router.get('/', (req, res) => {
       res.render('places/index', { places })
 })
+
+//SHOW page
+router.get('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else {
+    res.render('places/show', { place: places[id] })
+  }
+})
+
+
+
   
 module.exports = router
