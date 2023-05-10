d30e80cc5e15853c7152d649428cbe2a9a7efc02
Juan Mireles <119257668+Kal0yanX@users.noreply.github.com>
2023-05-10 17:31:55 -0400

    formatting

--- views/places/new.jsx
+++ views/places/new.jsx
@@ -16,25 +16,47 @@ function new_form (data) {
             <h1>Add a New Place</h1>
             {message}
             <form method="POST" action="/places">
-              <div className="form-group">
+            <div className="row">
+            <div className="form-group col-sm-6">
                 <label htmlFor="name">Place Name</label>
-                <input className="form-control" id="name" name="name" required />
+                <input 
+                className="form-control" 
+                id="name" 
+                name="name" 
+                required />
               </div>
-              <div className="form-group">
+              <div className="form-group col-sm-6">
                 <label htmlFor="pic">Place Picture</label>
-                <input className="form-control" type="url" id="pic" name="pic" />
+                <input 
+                className="form-control" 
+                type="url" 
+                id="pic" 
+                name="pic" />
               </div>
-              <div className="form-group">
+              </div>
+              <div className="row">
+              <div className="form-group col-sm-4">
                 <label htmlFor="city">City</label>
-                <input className="form-control" id="city" name="city" />
+                <input 
+                className="form-control" 
+                id="city" 
+                name="city" />
               </div>
-              <div className="form-group">
+              <div className="form-group col-sm-4">
                 <label htmlFor="state">State</label>
-                <input className="form-control" id="state" name="state" />
+                <input 
+                className="form-control" 
+                id="state" 
+                name="state" />
               </div>
-              <div className="form-group">
+              <div className="form-group col-sm-4">
                 <label htmlFor="cuisines">Cuisines</label>
-                <input className="form-control" id="cuisines" name="cuisines" required />
+                <input 
+                className="form-control" 
+                id="cuisines" 
+                name="cuisines" 
+                required />
+              </div>
               </div>
               <div className="form-group">
                 <label for="founded">Founded Year</label>
